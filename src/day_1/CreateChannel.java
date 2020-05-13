1d836f268d24976020f04e2d277419fe16b37e3e
CCicek <muhammetcicekster@gmail.com>
2020-05-13 15:03:59 +0000

    asdasd

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,7 +2,11 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
-
+        System.out.println("User is able to create channel");
+        int x = 0;
+        if (x == 0) {
+            System.out.println("equagitl");
+        }
     }
 }
 
